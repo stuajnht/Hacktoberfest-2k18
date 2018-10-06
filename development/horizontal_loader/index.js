2f3666f1592a17b89c2e3d2661d5b284c86b41fb
Jonathan Hart <stuajnht@users.noreply.github.com>
2018-10-06 22:19:20 +0100

    Started to create the `init` function

--- development/horizontal_loader/index.js
+++ development/horizontal_loader/index.js
@@ -8,3 +8,21 @@
  * Hide () - change opacity of overall bar to 0
  * Reset () - set the bar back to 0% and intermediate to 100%
  */
+
+ var horizontalLoader = ( function loader( window, document ) {
+   var loader__container;
+
+   return {
+     init: function ( element, options ) {
+       console.info( 'Using', element, 'for the loading bar' );
+
+       loader__container = document.getElementById( element );
+
+       if ( loader__container === null ) {
+         console.error( 'Unable to use', element, 'for the loading bar. Is this a valid element ID?' );
+       }
+
+       return loader__container;
+     }
+   };
+ }( window, document ) )
