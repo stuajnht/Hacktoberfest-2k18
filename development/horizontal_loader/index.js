d5bce8e85cead0b92ab6d6e7eba54b27912de760
Jonathan Hart <stuajnht@users.noreply.github.com>
2018-10-06 22:39:06 +0100

    Removed `show` and `hide` from the To-do list

--- development/horizontal_loader/index.js
+++ development/horizontal_loader/index.js
@@ -1,11 +1,9 @@
 /**
  * Todo:
  * Init (with options [show intermediate, intermediate amount of blocks, show glow, parent container, colours?])
- * Show () - change opacity of overall bar to 1
  * Set (position, duration(optional))
  *   - the duration can be achieved by applying the `transition-duration` style then removing it when complete
  *   - only return from this when the duration has been completed
- * Hide () - change opacity of overall bar to 0
  * Reset () - set the bar back to 0% and intermediate to 100%
  */
 
